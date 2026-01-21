7a420ba7ee11f1bb53a3643e46e5faf1519112db
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2026-01-21 21:01:01 +0300

    Update fedora-update.sh

--- fedora-update.sh
+++ fedora-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash/
 
 sudo dnf install dnf-plugin-system-upgrade #Install plugin for full upgrade
-sudo dnf system-upgrade download --releasever=38 #Upgrade to current Fedora Version
+sudo dnf system-upgrade download --releasever=43 #Upgrade to current Fedora Version
 sudo dnf system-upgrade reboot #Reboot and upgrade on splash screen
