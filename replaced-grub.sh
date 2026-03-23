d282b8236a96d1aa41052cc584ddb193da338204
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2026-03-23 18:44:42 +0300

    Trying to restore this script

---
+++ replaced-grub.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Name scipt: replaced-grub.sh
+# Copyright (c) Eat! Sleep! Linux Authors
+# Script based on https://kowalski7cc.xyz/blog/systemd-boot-fedora-32 and https://github.com/peterwu/dotfiles/blob/master/install/fedora.org#replace-grub-with-systemd-boot
+# Made by Georgii Bogdanov | gbog@proton.me
+
+### DON'T USE THIS SCRIPT. IT'S BROKE YOUR SYSTEM. WE INVESTIGATE ISSUES
+
+set -euxo pipefail
+
+sudo mkdir /efi
+sudo sed -i 's|/boot/efi|/efi|g' /etc/fstab
+sudo umount /boot/efi
+sudo mount /efi
+sudo mkdir /efi/$(cat /etc/machine-id)
+sudo rm /etc/dnf/protected.d/{grub*,shim.conf}
+sudo dnf remove grubby grub2\* shim\* memtest86\ && sudo rm -rf /boot/grub2 && sudo rm -rf /boot/loader
+cat /proc/cmdline | cut -d ' ' -f 2- | sudo tee /etc/kernel/cmdline
+sudo bootctl install
+sudo kernel-install add $(uname -r) /lib/modules/$(uname -r)/vmlinuz
+sudo dnf reinstall kernel-core
+systemctl reboot
