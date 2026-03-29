b09e4ff8dcd0489e92c55cdf8ff527ae3d45f50b
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2026-03-29 03:21:04 +0300

    Update replaced-grub.sh

--- replaced-grub.sh
+++ replaced-grub.sh
@@ -9,13 +9,18 @@ set -euxo pipefail
 
 sudo mkdir /efi
 sudo sed -i 's|/boot/efi|/efi|g' /etc/fstab
+sudo systemctl daemon-reload
 sudo umount /boot/efi
+sudo umount /boot
 sudo mount /efi
 sudo mkdir /efi/$(cat /etc/machine-id)
 sudo rm /etc/dnf/protected.d/{grub*,shim.conf}
-sudo dnf remove grubby grub2\* shim\* memtest86\ && sudo rm -rf /boot/grub2 && sudo rm -rf /boot/loader
+sudo dnf remove -y grubby grub2\* memtest86\* && sudo rm -rf /boot/*
+sudo dnf install -y systemd-boot-unsigned sdubby
+sudo sed -i 's|/boot/efi|/efi|g' /etc/kernel/install.conf
+sudo sed -i '5a layout=bls' /etc/kernel/install.conf
 cat /proc/cmdline | cut -d ' ' -f 2- | sudo tee /etc/kernel/cmdline
-sudo bootctl install
+sudo bootctl install --efi-boot-option-description="Fedora"
 sudo kernel-install add $(uname -r) /lib/modules/$(uname -r)/vmlinuz
 sudo dnf reinstall kernel-core
 systemctl reboot
